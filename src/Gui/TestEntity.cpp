0235655e41fd14fd3d9c2235395889e30ca22500
kim366 <kim.schmider@hotmail.com>
2017-07-09 16:41:06 +0200

    Remove commented out code

--- src/Gui/TestEntity.cpp
+++ src/Gui/TestEntity.cpp
@@ -12,8 +12,6 @@ void Gui::TestEntity::update(float delta_time_, const Inputs& inputs_)
 {
 	if (inputs_.event.pressed(sf::Mouse::Left))
 		std::clog << "LMB pressed\n";
-
-	// std::clog << "(" << inputs_.cursor_position.x << ", " << inputs_.cursor_position.y << ")\n";
 }
 
 void Gui::TestEntity::draw(sf::RenderTarget& target_, sf::RenderStates states_) const
