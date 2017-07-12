639102d592e773d085356ef65dfceab6025f30b4
kim366 <kim.schmider@hotmail.com>
2017-07-12 14:13:00 +0200

    Add Files for Key rebinder

---
+++ inc/Gui/BoundKeys.hpp
@@ -0,0 +1,26 @@
+#ifndef GUI_BOUND_KEYS_HPP
+#define GUI_BOUND_KEYS_HPP
+
+namespace Gui
+{
+
+namespace BoundKeys
+{
+
+enum Mouse
+{
+	Fill,
+	Clear,
+};
+
+enum AnyDevice
+{
+	FillAll,
+	ClearAll
+};
+
+}
+
+} // namespace Gui
+
+#endif // GUI_BOUND_KEYS_HPP
---
+++ inc/Gui/KeyRebinder.hpp
@@ -0,0 +1,18 @@
+#ifndef GUI_KEY_REBINDER_HPP
+#define GUI_KEY_REBINDER_HPP
+
+namespace Gui
+{
+
+class KeyRebinder
+{
+public:
+	
+
+private:
+	
+};
+
+} // namespace Gui
+
+#endif // GUI_KEY_REBINDER_HPP
