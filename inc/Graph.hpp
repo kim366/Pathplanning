36a98d3e0a0bcf6363769c671e0497f7154813fa
kim366 <kim.schmider@hotmail.com>
2017-07-25 17:08:40 +0200

    Add Getter functions for Node & Edge containers in Graph class

--- inc/Graph.hpp
+++ inc/Graph.hpp
@@ -10,17 +10,20 @@
 class Graph
 {
 public:
-	float	getWeight(Node* node1_, Node* node2_); // Invokes undefined behavior if edge non-existant
-	void 	connect(Node* node1_, Node* node2_);
-	void 	disconnect(Node* node1_, Node* node2_);
-	Node*	createNode(unsigned x_, unsigned y_);
-	void	deleteNode(Node* node_);
+	float		getWeight(Node* node1_, Node* node2_); // Invokes undefined behavior if edge non-existant
+	void 		connect(Node* node1_, Node* node2_);
+	void 		disconnect(Node* node1_, Node* node2_);
+	Node*		createNode(unsigned x_, unsigned y_);
+	void		deleteNode(Node* node_);
+
+	const auto& getNodes() { return _nodes; }
+	const auto& getEdges() { return _edges; }
 
 private:
 	std::vector<std::unique_ptr<Node>>
-			_nodes;
+				_nodes;
 	std::vector<std::unique_ptr<Edge>>
-			_edges;	
+				_edges;	
 };
 
 #endif // GRAPH_HPP
