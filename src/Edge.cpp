3fd4d6f7ba1b98a7fff564c237aae7fa59947a78
kim366 <kim.schmider@hotmail.com>
2017-07-25 12:37:13 +0200

    Fix Build Errors in Edge class

--- src/Edge.cpp
+++ src/Edge.cpp
@@ -1,9 +1,11 @@
+#include <math.h>
 #include <Edge.hpp>
+#include <Node.hpp>
 
-Edge(Node* node1_, Node* node2_)
+Edge::Edge(Node* node1_, Node* node2_)
 	: _node1(node1_)
 	, _node2(node2_)
 {
-	weight = std::hypot(node1_->_position.x - *node2_->_position.x,
-		*node1_->_position.y - node2_->_position.y);
+	weight = std::hypot(node1_->_position.x - node2_->_position.x,
+		node1_->_position.y - node2_->_position.y);
 }
