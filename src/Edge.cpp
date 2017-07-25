cd7897f5582207c64845642387cb7acc3bdb708e
kim366 <kim.schmider@hotmail.com>
2017-07-25 12:25:30 +0200

    Replace Dot Star with Arrow

--- src/Edge.cpp
+++ src/Edge.cpp
@@ -4,6 +4,6 @@ Edge(Node* node1_, Node* node2_)
 	: _node1(node1_)
 	, _node2(node2_)
 {
-	weight = std::hypot(*node1_._position.x - *node2_._position.x,
-		*node1_._position.y - *node2_._position.y);
+	weight = std::hypot(node1_->_position.x - *node2_->_position.x,
+		*node1_->_position.y - node2_->_position.y);
 }
