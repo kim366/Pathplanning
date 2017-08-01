5a8a2cb49827b6b8445fce507701fd70b364ff45
kim366 <kim.schmider@hotmail.com>
2017-08-01 10:24:22 +0200

    Add variables and return to AStarPathplanner::operator()

--- src/AStarPathplanner.cpp
+++ src/AStarPathplanner.cpp
@@ -3,6 +3,10 @@
 std::pair<std::vector<const Node*>, unsigned>
 	AStarPathplanner::operator()(const Node* start_, const Node* end_)
 {
+	std::vector<const Node*> found_path;
+	unsigned amount_of_expanded_nodes;
+
+	return std::make_pair(found_path, amount_of_expanded_nodes);
 }
 
 bool AStarPathplanner::evaluate(const Node* node1_, const Node* node2_)
