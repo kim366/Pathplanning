963dd90a2da08474e01b005a449923c63969e4c0
kim366 <kim.schmider@hotmail.com>
2017-08-14 10:18:12 +0200

    Reset node highlighing of expanded nodes

--- src/AStarVisualizer.cpp
+++ src/AStarVisualizer.cpp
@@ -16,6 +16,9 @@ void AStarVisualizer::update(float delta_time_, const Inputs& inputs_)
 {
 	if (inputs_.event.released(sf::Keyboard::Space))
 	{
+		for (auto& expanded_node : _result.second)
+			const_cast<Node*>(expanded_node)->status = Node::Standard;
+
 		if (_result.first)
 			for (auto& node_on_path : *_result.first)
 				const_cast<Node*>(node_on_path)->status = Node::Standard;
