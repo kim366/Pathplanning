5495e8ebb36a6faed832abc519813d4a7526360c
kim366 <kim.schmider@hotmail.com>
2017-10-27 12:30:25 +0200

    Move comparison lambda of pathplanner to .cpp file and fix indentation

--- include/Pathplanner.hpp
+++ include/Pathplanner.hpp
@@ -21,29 +21,22 @@ struct EvaluationReturnType
 class Pathplanner
 {
 public:
-	virtual PathplanningReturnType 
-					operator()(const Node* start_, const Node* goal_) = 0;
-	virtual			~Pathplanner() = 0;
+											Pathplanner();
+	virtual									~Pathplanner() = 0;
+
+	virtual PathplanningReturnType 			operator()(const Node* start_, const Node* goal_) = 0;
 
 protected:
-	virtual EvaluationReturnType
-					evaluate(const Node* to_evaluate_, const Node* based_on_) const = 0;
-	float			cost(const Node* from_, const Node* to_) const;
+	virtual EvaluationReturnType			evaluate(const Node* to_evaluate_, const Node* based_on_) const = 0;
+	float									cost(const Node* from_, const Node* to_) const;
 
 protected:
-	const std::function<bool(Node*, Node*)>
-					_compare{[this] (Node* x_, Node* y_)
-					{
-						sf::Vector2f value{x_->getPathplanningData().value, y_->getPathplanningData().value};
-						if (value.x == value.y && x_ == _goal)
-							return false;
-						return value.x > value.y;
-					}};
+	const std::function<bool(Node*, Node*)>	_compare;
 					
 	std::priority_queue<Node*, std::vector<Node*>, decltype(_compare)>
-					_open{_compare};
-	const Node*		_start;
-	const Node*		_goal;
+											_open{_compare};
+	const Node*								_start;
+	const Node*								_goal;
 
 public:
 	enum Tag
--- src/Pathplanner.cpp
+++ src/Pathplanner.cpp
@@ -1,5 +1,16 @@
 #include <Pathplanner.hpp>
 
+Pathplanner::Pathplanner()
+	: _compare{[this] (Node* x_, Node* y_)
+	{
+		sf::Vector2f value{x_->getPathplanningData().value, y_->getPathplanningData().value};
+		if (value.x == value.y && x_ == _goal)
+			return false;
+		return value.x > value.y;
+	}}
+{
+}
+
 Pathplanner::~Pathplanner()
 {
 }
