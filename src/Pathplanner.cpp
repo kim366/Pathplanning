e949cd35724366812b72a7501b587d3703ece8ab
kim366 <kim.schmider@hotmail.com>
2017-11-06 21:42:23 +0100

    Fix priority queue compare lambda

--- src/Pathplanner.cpp
+++ src/Pathplanner.cpp
@@ -1,12 +1,11 @@
 #include <Pathplanner.hpp>
 
 Pathplanner::Pathplanner()
-	: _compare{[this] (Node* x_, Node* y_)
+	: _compare{[this] (Node* topmost_, Node* newly_added_)
 	{
-		sf::Vector2f value{x_->getPathplanningData().value, y_->getPathplanningData().value};
-		if (value.x == value.y && x_ == _goal)
-			return false;
-		return value.x > value.y;
+		if (topmost_->getPathplanningData().value == newly_added_->getPathplanningData().value)
+			return newly_added == _goal;
+		return newly_added_ < topmost_;
 	}}
 {
 }
