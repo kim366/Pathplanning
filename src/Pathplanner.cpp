789e2a66876c9b240e840dbf51768096bdc26f4c
kim366 <kim.schmider@hotmail.com>
2017-11-10 14:29:00 +0100

    Fix bug comparing addresses instead of values

--- src/Pathplanner.cpp
+++ src/Pathplanner.cpp
@@ -1,11 +1,11 @@
 #include <Pathplanner.hpp>
 
 Pathplanner::Pathplanner()
-	: _compare{[this] (Node* topmost_, Node* newly_added_)
+	: _compare{[this] (Node* topmost_, Node* newly_added_) -> bool
 	{
 		if (topmost_->getPathplanningData().value == newly_added_->getPathplanningData().value)
 			return newly_added_ == _goal;
-		return newly_added_ < topmost_;
+		return newly_added_->getPathplanningData().value < topmost_->getPathplanningData().value;
 	}}
 {
 }
