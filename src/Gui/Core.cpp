4734bcce1cfadc2f213a182c2b5414b48b595d1a
kim366 <kim.schmider@hotmail.com>
2018-01-28 12:28:53 +0100

    Prepare grid for performance testing

--- src/Gui/Core.cpp
+++ src/Gui/Core.cpp
@@ -11,7 +11,7 @@ namespace Gui
 
 Core::Core()
 {
-	const int grid_size{10};
+	const int grid_size{50};
 	auto grid{std::make_unique<Grid>(grid_size, true)};
 	// grid->disconnect({grid->toIndex({4, 3}), grid->toIndex({4, 2})});
 	// grid->disconnect({grid->toIndex({5, 3}), grid->toIndex({5, 2})});
@@ -24,40 +24,41 @@ Core::Core()
 	// grid->disconnect({grid->toIndex({6, 3}), grid->toIndex({7, 2})});
 	// grid->disconnect({grid->toIndex({6, 4}), grid->toIndex({7, 3})});
 	// grid->disconnect({grid->toIndex({6, 5}), grid->toIndex({7, 4})});
-	std::unique_ptr<PathplannerVisualizer> visualizer{new PathplannerVisualizer{std::make_unique<DStarPathplanner>(*grid), *grid, (*grid)[grid->toIndex({1, 8})], (*grid)[grid->toIndex({8, 1})]}};
+	// std::unique_ptr<PathplannerVisualizer> visualizer{new PathplannerVisualizer{std::make_unique<DStarPathplanner>(*grid), *grid, (*grid)[grid->toIndex({1, 8})], (*grid)[grid->toIndex({8, 1})]}};
 
 	// grid.disconnect({grid.toIndex({8, 3}), grid.toIndex({8, 4})});
 	// grid.disconnect({grid.toIndex({8, 1}), grid.toIndex({8, 2})});
 	// grid.disconnect({grid.toIndex({7, 3}), grid.toIndex({8, 4})});
 
-	grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({4, 4})});
-	grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({4, 3})});
-	grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({5, 5})});
-	grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({5, 3})});
-	grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({6, 5})});
-	grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({6, 4})});
-	grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({6, 3})});
+	// grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({4, 4})});
+	// grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({4, 3})});
+	// grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({5, 5})});
+	// grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({5, 3})});
+	// grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({6, 5})});
+	// grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({6, 4})});
+	// grid->disconnect({grid->toIndex({5, 4}), grid->toIndex({6, 3})});
 
-	// std::unique_ptr<PathplannerVisualizer> visualizer{new PathplannerVisualizer{std::make_unique<DStarPathplanner>(/*[] (...) { return 0; }, */*grid), *grid, (*grid)[grid->toIndex({16, 33})], (*grid)[grid->toIndex({33, 16})]}};
+	std::unique_ptr<PathplannerVisualizer> visualizer{new PathplannerVisualizer{std::make_unique<DStarPathplanner>(/*[] (...) { return 0; }, */*grid), *grid, (*grid)[grid->toIndex({16, 33})], (*grid)[grid->toIndex({33, 16})]}};
 
-	// std::mt19937 rng{std::random_device{}()};
-	// std::normal_distribution random_node{25., 10.};
+	std::mt19937 rng{std::random_device{}()};
+	rng.seed(1);
+	std::normal_distribution random_node{25., 10.};
 
-	// for (int counter{0}; counter < 5000; ++counter)
-	// {
-	// 	int chosen_node_index(grid->toIndex({std::clamp<int>(random_node(rng), 0, grid_size - 1), std::clamp<int>(random_node(rng), 0, grid_size - 1)}));
+	for (int counter{0}; counter < 5000; ++counter)
+	{
+		int chosen_node_index(grid->toIndex({std::clamp<int>(random_node(rng), 0, grid_size - 1), std::clamp<int>(random_node(rng), 0, grid_size - 1)}));
 
-	// 	for (int second_counter{0}; second_counter < 3; ++second_counter)
-	// 	{
-	// 		if ((*grid)[chosen_node_index]->neighbors.size() <= 3)
-	// 			break;
+		for (int second_counter{0}; second_counter < 2; ++second_counter)
+		{
+			if ((*grid)[chosen_node_index]->neighbors.size() <= 7)
+				break;
 
-	// 		std::uniform_int_distribution<int> random_neighbor(0, (*grid)[chosen_node_index]->neighbors.size() - 1);
-	// 		auto neighbor_it{(*grid)[chosen_node_index]->neighbors.begin()};
-	// 		std::advance(neighbor_it, random_neighbor(rng));
-	// 		grid->disconnect({chosen_node_index, neighbor_it->first.getIndex()});
-	// 	}
-	// }
+			std::uniform_int_distribution<int> random_neighbor(0, (*grid)[chosen_node_index]->neighbors.size() - 1);
+			auto neighbor_it{(*grid)[chosen_node_index]->neighbors.begin()};
+			std::advance(neighbor_it, random_neighbor(rng));
+			grid->disconnect({chosen_node_index, neighbor_it->first.getIndex()});
+		}
+	}
 
 	_entity_manager->addEntity(std::move(grid));
 	_entity_manager->addEntity(std::move(visualizer));
